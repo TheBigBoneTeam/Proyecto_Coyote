9a117f92de5dff95a1334b12690b2516674b05be
VoladorSapo <voladorsapo@gmail.com>
2025-10-04 12:34:36 +0200

    error raro solucionado

--- ProyectoCoyote/Assets/Editor/EAttackDataEditor.cs
+++ ProyectoCoyote/Assets/Editor/EAttackDataEditor.cs
@@ -18,4 +18,4 @@ public class EAttackDataEditor : Editor
             data.AddStun();
         }
     }
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                     

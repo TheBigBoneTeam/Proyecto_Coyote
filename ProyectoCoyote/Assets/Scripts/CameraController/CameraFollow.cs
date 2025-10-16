48d7b2af63d77606e090a7b1031d748afdb4db8b
Andyy728 <andrealb0112@gmail.com>
2025-10-16 18:06:20 +0200

    Camera follow

--- ProyectoCoyote/Assets/Scripts/CameraController/CameraFollow.cs
+++ ProyectoCoyote/Assets/Scripts/CameraController/CameraFollow.cs
@@ -6,94 +6,61 @@ using UnityEngine;
 // lockeada o no rotará libremente o alrededor del enemigo
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    [SerializeField] Transform enemyTarget;
-    [SerializeField] Vector3 offset;
-    [SerializeField] Vector2 clampAxis = new Vector2 (60, 60);
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+    public Transform enemyTarget;
 
-    [SerializeField] float follow_smoothing = 5.0f;
-    [SerializeField] float rotate_smoothing = 5.0f;
-    [SerializeField] float sensitivity = 60;
-
-    float rotx, roty;
-    bool cursorLocked = false; //// Input System
-    Transform cam;
-
+    [SerializeField] Vector2 clampAxis = new Vector2(60, 60);
     public bool lockedTarget;
+    public float rotationSpeed;
+    
 
-    void Start()
+    private void Start()
     {
-        //// Input System
-        Cursor.visible = false; 
         Cursor.lockState = CursorLockMode.Locked;
-        ////
-        cam = Camera.main.transform;
+        Cursor.visible = false;
     }
 
-    void Update()
+    private void Update()
     {
-        // Definir posición del objetivo y transformar su posición en función de éste
-        Vector3 target_P = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, target_P, follow_smoothing*Time.deltaTime);
+        // Rotar orientacion
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
 
-        // Condición de si está lockeado o no
-        if (!lockedTarget) CameraTargetRotation(); else LookAtTarget();
-
-        //// Input System
-        if (Input.GetKeyDown(KeyCode.Escape)) 
-        {
-            if (cursorLocked)
-            {
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
-            }
-            else 
-            {
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
-            }
-        }
-        ////
-    }
-    
-    ////Input System
-    Vector2 InputCamera()
-    {
-        Vector2 axis = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        return axis;
-    }
-    ////
-    
-    void CameraTargetRotation()
-    {
-        Vector2 axis = InputCamera();
-        rotx += (axis.x * sensitivity) * Time.deltaTime;
-        roty += (axis.y * sensitivity) * Time.deltaTime;
-
-        roty = Mathf.Clamp(roty, clampAxis.x, clampAxis.y);
-
-        Quaternion camRotation = Quaternion.Euler(roty, rotx, 0);
-        transform.rotation = Quaternion.Slerp(transform.rotation, camRotation, Time.deltaTime * rotate_smoothing);
-
+        if (!lockedTarget) UnlockedCamera(viewDir); else LookAtTarget();
         
 
     }
 
+    public void UnlockedCamera(Vector3 viewDir)
+    {
+        // Rotar personaje
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        verticalInput = Math.Clamp(verticalInput, clampAxis.x, clampAxis.y);
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDir != Vector3.zero)
+        {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, viewDir.normalized, Time.deltaTime * rotationSpeed);
+        }
+    }
     void LookAtTarget()
     {
         if (enemyTarget == null) return;
 
         Vector3 dir = enemyTarget.position - transform.position;
-        dir.y = 0;
 
         if (dir.sqrMagnitude > 0.01f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(dir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotate_smoothing * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
-
-        Vector3 euler = transform.eulerAngles;
-        rotx = euler.y;
-        roty = euler.x;
     }
+
+
 }

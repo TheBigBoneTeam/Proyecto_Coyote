8b43b47e92500b82282e1a61942836478ee3b793
Andyy728 <andrealb0112@gmail.com>
2025-11-13 15:35:14 +0100

    Limpia consola

--- ProyectoCoyote/Assets/Scripts/DialogSystem/NPC.cs
+++ ProyectoCoyote/Assets/Scripts/DialogSystem/NPC.cs
@@ -5,10 +5,14 @@ public class NPC : MonoBehaviour
 {
     [SerializeField] string startingLine;
     [SerializeField] StoryAction action;
+    [SerializeField] float noticeZone;
+    Transform player;
     Dialogues dialogue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        player = FindAnyObjectByType<Player>().transform;
+
         dialogue = FindAnyObjectByType<Dialogues>();
 
     }
@@ -16,10 +20,25 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (LookForPlayer())
         {
             Debug.Log("Comenzando Dialogo..");
             dialogue.StartDialogue(startingLine, () => action.Execute(null));
         }
+    }
+
+    // Escanear alrededores en busca de enemigos
+    private bool LookForPlayer()
+    {
+        
+
+        if (Physics.OverlapSphere(transform.position, noticeZone, player.gameObject.layer) != null)
+        {
+            Debug.Log("Player encontrado");
+            return true; 
+        }
+            
+
+        return false;
     }
 }

d5b0850d09c1c8103d6647faf741e67a91728433
xinoJuegos <a.bernal.2021@alumnos.urjc.es>
2025-11-13 14:47:59 +0100

    MUSICA DINAMICA en niveles 1 y 2
    Modificar volumenes y percusion (tal vez quitar la percusion en la base para dar mas sensacion de cambio al combate)

--- ProyectoCoyote/Assets/Scripts/AudioManager/AudioManager.cs
+++ ProyectoCoyote/Assets/Scripts/AudioManager/AudioManager.cs
@@ -135,14 +135,23 @@ public class AudioManager : MonoBehaviour
                 AudioManager.Instance.PlaySimpleSoundFadeIn(0f, "OST Cañon - Pelea", true, Vector2.zero, true, true, 1);
                 if (musicSounds[1] != null)
                     musicSounds[1].audioSource.volume = 0f;
+
                 break;
 
             case "Nivel1":
-                AudioManager.Instance.PlaySimpleSoundFadeIn(2f, "OST Cañon - Base", true, Vector2.zero, true, true);
+                AudioManager.Instance.PlaySimpleSoundFadeIn(2f, "OST Cañon - Base", true, Vector2.zero, true, true, 0);
+                AudioManager.Instance.PlaySimpleSoundFadeIn(0f, "OST Cañon - Pelea", true, Vector2.zero, true, true, 1);
+                if (musicSounds[1] != null)
+                    musicSounds[1].audioSource.volume = 0f;
+
                 break;
 
             case "Nivel2":
-                AudioManager.Instance.PlaySimpleSoundFadeIn(2f, "OST Cañon - Base", true, Vector2.zero, true, true);
+                AudioManager.Instance.PlaySimpleSoundFadeIn(2f, "OST Cañon - Base", true, Vector2.zero, true, true, 0);
+                AudioManager.Instance.PlaySimpleSoundFadeIn(0f, "OST Cañon - Pelea", true, Vector2.zero, true, true, 1);
+                if (musicSounds[1] != null)
+                    musicSounds[1].audioSource.volume = 0f;
+
                 break;
 
             case "TesteoCInematicas":

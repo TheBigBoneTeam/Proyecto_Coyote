23a64604625ae11ba2e69979d32699494f2c16b9
VoladorSapo <voladorsapo@gmail.com>
2025-11-19 18:56:25 +0100

    Hitstop Para edu

--- ProyectoCoyote/Assets/Scripts/Combat/HitStopComponent.cs
+++ ProyectoCoyote/Assets/Scripts/Combat/HitStopComponent.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.TextCore.Text;
 
 public class HitStopComponent : AHitstopBase
 {
     private Rigidbody _rigidbody;
     Vector3 _storedVelocity,_storedAngVelocity;
-    
+    Animator[] animators;
+    float[] savedAnimSpeed;
+    NavMeshAgent agent;
+    float navMeshSpeed;
     public override void Start()
     {
         base.Start();
         _rigidbody = GetComponent<Rigidbody>();
+        animators = GetComponentsInChildren<Animator>();
+        savedAnimSpeed = new float[animators.Length];
+        agent = GetComponent<NavMeshAgent>();
     }
     protected override void Stop()
     {
@@ -18,6 +25,16 @@ public class HitStopComponent : AHitstopBase
         _rigidbody.linearVelocity = Vector3.zero;
         _rigidbody.angularVelocity = Vector3.zero;
         _rigidbody.isKinematic = true;
+        if (animators != null)
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                savedAnimSpeed[i] = animators[i].speed;
+                animators[i].speed = 0;
+            }
+        }
+        navMeshSpeed = agent.speed;
+        agent.speed = 0;
        // gameObject.GetComponent<Renderer>().material.color = new Color(138f/255, 43f / 255, 226f / 255, 1);
 
         print("stop");
@@ -28,7 +45,19 @@ public class HitStopComponent : AHitstopBase
         _rigidbody.isKinematic = false;
         _rigidbody.angularVelocity = _storedAngVelocity;
         _rigidbody.linearVelocity= _storedVelocity;
-       // gameObject.GetComponent<Renderer>().material.color = Color.gray;
+        // gameObject.GetComponent<Renderer>().material.color = Color.gray;
+        if (animators != null)
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                animators[i].speed = savedAnimSpeed[i];
+            }
+        }
+        agent.speed = navMeshSpeed;
+
+    }
+    private void Update()
+    {
 
     }
 }

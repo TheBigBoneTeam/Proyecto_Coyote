d82d1bd12bc8057cd5c8d762c11d3a671eaba917
Andyy728 <andrealb0112@gmail.com>
2026-01-08 20:26:48 +0100

    Cursor al volver y correccion errata

--- ProyectoCoyote/Assets/Scripts/Menus/MenuReturn.cs
+++ ProyectoCoyote/Assets/Scripts/Menus/MenuReturn.cs
@@ -6,6 +6,8 @@ public class MenuReturn : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void ReturnToMenu()
     {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("menuGold");
     }
 
